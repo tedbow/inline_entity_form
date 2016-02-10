baaf95f3988f36df936dc38e45738c0afa2c6cea
Ted Bowman <ted.git@tedbow.com>
2016-02-10 02:10:08 +0000

    add more complex form element tests

--- src/Tests/InlineEntityFormSimpleWebTest.php
+++ src/Tests/InlineEntityFormSimpleWebTest.php
@@ -8,10 +8,9 @@
 namespace Drupal\inline_entity_form\Tests;
 
 use Drupal\Core\Field\FieldStorageDefinitionInterface;
-use Drupal\simpletest\WebTestBase;
 
 /**
- * Tests the IEF element on a custom form.
+ * Tests the IEF simple widget.
  *
  * @group inline_entity_form
  */
@@ -54,24 +53,6 @@ class InlineEntityFormWebTest extends InlineEntityFormTestBase {
     $this->fieldStorageConfigStorage = $this->container
       ->get('entity_type.manager')
       ->getStorage('field_storage_config');
-  }
-
-  /**
-   * Tests IEF on a custom form.
-   */
-  public function testCustomFormIEF() {
-    $this->drupalGet('ief-test');
-    $this->assertText(t('Title'), 'Title field found on the form.');
-
-    $edit = ['inline_entity_form[title][0][value]' => $this->randomString()];
-    $this->drupalPostForm('ief-test', $edit, t('Save'));
-    $message = t('Created @entity_type @label.', ['@entity_type' => t('Content'), '@label' => $edit['inline_entity_form[title][0][value]']]);
-    $this->assertText($message, 'Status message found on the page.');
-
-    /** @var \Drupal\node\NodeInterface $node */
-    $node = $this->container->get('entity.manager')->getStorage('node')->load(1);
-    $this->assertEqual($node->label(), $edit['inline_entity_form[title][0][value]'], 'Node title correctly saved to the database.');
-    $this->assertEqual($node->bundle(), 'ief_test_custom', 'Correct bundle used when creating the new node.');
   }
 
   /**
--- src/Tests/InlineEntityFormTestBase.php
+++ src/Tests/InlineEntityFormTestBase.php
@@ -38,4 +38,42 @@ abstract class InlineEntityFormTestBase extends WebTestBase{
     return $retval;
   }
 
+  /**
+   * Passes if no node is found for the title.
+   *
+   * @param $title
+   *   Node title to check.
+   * @param $message
+   *   Message to display.
+   */
+  protected function assertNoNodeByTitle($title, $message = '') {
+    if (!$message) {
+      $message = "No node with title: $title";
+    }
+    $node = $this->getNodeByTitle($title);
+    $this->assert('pass', "node = " . $node);
+
+    $this->assertTrue(empty($node), $message);
+  }
+
+  /**
+   * Passes if node is found for the title.
+   *
+   * @param $title
+   *   Node title to check.
+   * @param $message
+   *   Message to display.
+   */
+  protected function assertNodeByTitle($title, $bundle = NULL, $message = '') {
+    if (!$message) {
+      $message = "Node with title found: $title";
+    }
+    $node = $this->getNodeByTitle($title);
+    if ($this->assertTrue(!empty($node), $message)) {
+      if ($bundle) {
+        $this->assertEqual($node->bundle(), $bundle, "Node is correct bundle: $bundle");
+      }
+    }
+  }
+
 }
